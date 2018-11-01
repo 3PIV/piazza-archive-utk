53fa10c8c961c7f9b292493265cf524d65fdb1d5
Preston Provins <pprovins@vols.utk.edu>
2018-11-01 10:58:01 -0400

    Update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const router = express.Router();
 
 // set our port to either a predetermined port number if you have set it up, or 3001
-const API_PORT = process.env.API_PORT || 3001;
+const API_PORT = process.env.PORT || 3001;
 
 mongoose.connect(getSecret('dbUri'));
 var db = mongoose.connection;
