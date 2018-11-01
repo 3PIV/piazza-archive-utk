11d2a0dd696933b9b512fee0f30589b862ded5f9
Jerry <mikeynjerry@gmail.com>
2018-10-31 21:27:21 -0400

    Display Post Data

--- client/src/PostContent.js
+++ client/src/PostContent.js
@@ -6,7 +6,21 @@ import PropTypes from 'prop-types'
 const PostContent = ({ postdata }) => {
     return (
         <div>
-            <h1>{postdata.id}</h1>
+            <h1><code>{postdata.history && postdata.history.length && postdata.history[0].content}</code></h1>
+            {postdata.children && postdata.children.length && postdata.children.map((comment, i) => (
+              <div key={i}>
+                2nd level {(comment.type === "s_answer" || comment.type === "i_answer") ? (
+                  <code>{comment.history && comment.history.length && comment.history[0].content}</code>
+                ) : (
+                  <code>{comment.subject}</code>
+                )}
+                {comment.children.length ? comment.children.map((reply, i) => (
+                  <div key={i}>
+                    3rd level {reply.type === "feedback" && (<code>{reply.subject}</code>)}
+                  </div>
+                )) : null}
+              </div>
+            ))}
         </div>
     );
 };
